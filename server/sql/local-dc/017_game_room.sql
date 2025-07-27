168601520b3b761cb6ce7d195a69ce3406cd1ebf
Alexander Skripnikov <alexskr@proton.me>
2025-07-27 10:33:20 +0300

    Game rooms for online gaming

---
+++ server/sql/local-dc/017_game_room.sql
@@ -0,0 +1,10 @@
+-- +goose Up
+-- game room for online gaming
+create table game_room (
+    id UUID primary key default gen_random_uuid(),
+    name varchar(255) not null,
+    owner_id UUID not null references users(id) -- owner is always a game master
+);
+
+-- +goose Down
+drop table game_room;
---
+++ server/sql/local-dc/018_game_room_participants.sql
@@ -0,0 +1,9 @@
+-- +goose Up
+create table game_room_participants(
+    id UUID primary key default gen_random_uuid(),
+    user_id UUID references users(id),
+    game_room_id UUID references game_rooms(id)
+);
+
+-- +goose Down
+drop table game_room_participants;
